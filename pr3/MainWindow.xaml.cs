b39a7b5482b642f802906c7f890d0caf8aef6878
Nikita Volzhin <vlzh.nik@mail.ru>
2024-12-18 22:18:02 +0500

    test

---
+++ pr3/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+
+namespace pr3;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public List<Product> Products { get; set;}
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        Products = new List<Product>();
+        ProductsDataGrid.ItemsSource = Products;
+    }
+
+    private void AddProductButton_Click(object sender, RoutedEventArgs e)
+    {
+        string productName = ProductNameTextBox.Text;
+        string productPrice = ProductPriceTextBox.Text;
+        string productQuantity = ProductQuantityTextBox.Text;
+        
+        Products.Add(new Product
+        {
+            ProductName = productName,
+            ProductPrice = productPrice,
+            ProductQuantity = productQuantity
+        });
+        
+        ProductNameTextBox.Clear();
+        ProductPriceTextBox.Clear();
+        ProductQuantityTextBox.Clear();
+    }
+
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public string ProductPrice { get; set; }
+        public string ProductQuantity { get; set; }
+    }
+}
